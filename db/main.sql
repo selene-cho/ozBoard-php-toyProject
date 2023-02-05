5e926c6efe50dbbaabc7d83352055d8e73e9dc8e
yesaroun <akor1@naver.com>
2023-02-05 13:06:18 +0900

    create tables

---
+++ db/main.sql
@@ -0,0 +1,139 @@
+show databases;
+
+create database oz_php_board;
+
+use oz_php_board;
+
+show tables;
+
+create table member
+(
+    unq_id int auto_increment primary key,
+    pw char(20) not null ,
+    name char(20) not null ,
+    email char(20) not null ,
+    regi_day date not null ,
+    lv int not null default 1
+);
+
+
+create table notice
+(
+    noti_unq_id int auto_increment primary key,
+    user_unq_id int,
+    noti_title char(100) not null,
+    noti_regi_date datetime not null,
+    noti_edit_date datetime,
+    foreign key (user_unq_id)
+    references member(unq_id) on update cascade on delete set null
+);
+
+create table notice_content
+(
+    noti_cnt_unq_id int auto_increment primary key,
+    noti_unq_id int not null,
+    noti_cnt text not null,
+    noti_is_html boolean,
+    foreign key (noti_unq_id)
+    references notice(noti_unq_id) on update cascade on delete cascade
+);
+
+select * from notice_content;
+drop table notice_content;
+
+create table notice_comment
+(
+    noti_cmt_unq_id int auto_increment primary key,
+    user_unq_id int,
+    noti_unq_id int not null,
+    noti_cmt_cnt text not null,
+    noti_cmt_regi_date datetime not null,
+    noti_cmt_edit_date datetime,
+    foreign key (user_unq_id)
+    references member(unq_id) on update cascade on delete set null,
+    foreign key (noti_unq_id)
+    references notice(noti_unq_id) on update cascade on delete cascade
+);
+
+create table qna
+(
+    qna_unq_id int auto_increment primary key,
+    user_unq_id int,
+    qna_title char(100) not null,
+    qna_regi_date datetime not null,
+    qna_edit_date datetime,
+    foreign key (user_unq_id)
+    references member(unq_id) on update cascade on delete set null
+);
+
+create table qna_recommend
+(
+    qna_rcmd_unq_id int auto_increment primary key,
+    user_unq_id int,
+    qna_unq_id int not null,
+    foreign key (user_unq_id)
+    references member(unq_id) on update cascade on delete set null,
+    foreign key (qna_unq_id)
+    references qna(qna_unq_id) on update cascade on delete cascade
+);
+
+create table qna_content
+(
+    qna_cnt_unq_id int auto_increment primary key,
+    qna_unq_id int not null,
+    noti_cnt text not null,
+    noti_is_html boolean default(false) not null,
+    foreign key (qna_unq_id)
+    references qna(qna_unq_id) on update cascade on delete cascade
+);
+
+create table qna_comment
+(
+    qna_cmt_unq_id int auto_increment primary key,
+    user_unq_id int,
+    qna_unq_id int not null,
+    qna_cmt_cnt text not null,
+    qna_cmt_regi_date datetime not null,
+    qna_cmt_edit_date datetime,
+    foreign key (user_unq_id)
+    references member(unq_id) on update cascade on delete set null,
+    foreign key (qna_unq_id)
+    references qna(qna_unq_id) on update cascade on delete cascade
+);
+
+create table qna_comment_recommend
+(
+    qna_cmt_rcmd_unq_id int auto_increment primary key,
+    user_unq_id int,
+    qna_cmt_unq_id int not null ,
+    foreign key (user_unq_id)
+    references member(unq_id) on update cascade on delete set null,
+    foreign key (qna_cmt_unq_id)
+    references qna_comment(qna_cmt_unq_id) on update cascade on delete cascade
+);
+
+create table simple_board
+(
+    sb_unq_id int auto_increment primary key,
+    user_unq_id int,
+    sb_cat char(10) not null,
+    sb_cnt text not null,
+    sb_regi_date datetime not null,
+    sb_edit_date datetime,
+    foreign key (user_unq_id)
+    references member(unq_id) on update cascade on delete set null
+);
+
+create table simple_board_comment
+(
+    sb_cmt_unq_id int auto_increment primary key,
+    user_unq_id int,
+    sb_unq_id int not null,
+    sb_cmt_cnt text not null,
+    sb_cmt_regi_date datetime not null,
+    sb_cmt_edit_date datetime,
+    foreign key (user_unq_id)
+    references member(unq_id) on update cascade on delete set null,
+    foreign key (sb_unq_id)
+    references simple_board(sb_unq_id) on update cascade on delete cascade
+);
